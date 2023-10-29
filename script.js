8bbe63a7ab2c0183eaf44acadbc9a6a9a337ed33
tommzhaanghg <49993277+tommzhaanghg@users.noreply.github.com>
2023-10-29 13:02:18 +0000

    add the feature of saving history

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+// Load existing papers from LocalStorage when the page loads
+window.onload = function() {
+    var papers = JSON.parse(localStorage.getItem('papers')) || [];
+    displayPapers(papers);
+}
+
+// Function to add a new paper
 function addPaper() {
     var title = document.getElementById('paper-title').value;
     var date = document.getElementById('paper-date').value;
     
     if (title && date) {
-        var papersList = document.getElementById('papers-list');
-        var li = document.createElement('li');
-        li.textContent = title + ' - ' + date;
-        papersList.appendChild(li);
+        // Get existing papers from LocalStorage or initialize to an empty array if null
+        var papers = JSON.parse(localStorage.getItem('papers')) || [];
+        
+        // Add new paper to the papers array
+        papers.push({ title: title, date: date });
+        
+        // Save updated papers array to LocalStorage
+        localStorage.setItem('papers', JSON.stringify(papers));
+        
+        // Update the displayed papers list
+        displayPapers(papers);
     }
+}
+
+// Function to display papers
+function displayPapers(papers) {
+    var papersList = document.getElementById('papers-list');
+    papersList.innerHTML = '';  // Clear the list
+    
+    papers.forEach(function(paper) {
+        var li = document.createElement('li');
+        li.textContent = paper.title + ' - ' + paper.date;
+        papersList.appendChild(li);
+    });
 }
